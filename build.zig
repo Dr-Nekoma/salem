df2f28967dbdc15c0bb97f63d24a4124fbb6b857
Marinho <marinho.pinotti@gmail.com>
2024-08-04 17:26:20 -0300

    Update build.zig for 0.12.0 and above

--- build.zig
+++ build.zig
@@ -9,7 +9,7 @@ pub fn build(b: *std.Build) void {
     const exe = b.addExecutable(.{
         .name = "salem",
         .pic = true,
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
     });
@@ -25,7 +25,7 @@ pub fn build(b: *std.Build) void {
     run_step.dependOn(&run_cmd.step);
 
     const unit_tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/main.zig" },
+        .root_source_file = b.path("src/main.zig"),
         .pic = true,
         .target = target,
         .optimize = optimize,
